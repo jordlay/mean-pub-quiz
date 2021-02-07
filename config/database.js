45a7998f3e7a5709d6dbb03bbde8def3857a1430
Jordan <jll541@york.ac.uk>
2021-02-07 17:08:47 +0000

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://jll541:mean-quiz@clusterquiz.inacn.mongodb.net/test',
+    database: "mongodb+srv://jll541:mean-quiz@clusterquiz.inacn.mongodb.net/quizdb?retryWrites=true&w=majority",
     secret: 'yoursecret'
 }
