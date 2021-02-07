b2cdca7ff048bddae6ee524963bb27d93a3e8bf8
Jordan <jll541@york.ac.uk>
2021-02-07 18:01:25 +0000

    replaced URL w actual string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: process.env.MONGOD_URI,
+    mongoURI: 'mongodb+srv://jll541:mean-quiz@clusterquiz.inacn.mongodb.net/quizdb?retryWrites=true&w=majority',
     database: 'mongodb://localhost:27017/quizdb',
     secret: 'yoursecret'
 }
