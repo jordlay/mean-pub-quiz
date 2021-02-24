9302e69138e0e3bb5ed1051d6ebfc09b638cdbce
jll541 <jll541@york.ac.uk>
2021-02-24 18:53:26 +0000

    added jitsi meeting to homeComp

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component, ElementRef, ViewChild } from '@angular/core';
+// import '../vendor/jitsi/external_api.js';
+import '../../../vendor/jitsi/external_api.js';
+declare var JitsiMeetExternalAPI: any;
 
 @Component({
   selector: 'app-home',
@@ -8,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+    //
+  }
+  @ViewChild('meet') meet: ElementRef | any;
+  public nickName = 'J';
+  private api: any;
+  gameStarted = false;
+
+
+  
+  startRoom(){
+    this.gameStarted = true;
+    const options = { roomName: 'JordansRoom',  width: 700, height: 700, parentNode: this.meet.nativeElement};
+    const domain = 'meet.jit.si';
+    this.api = new JitsiMeetExternalAPI(domain, options);
+    console.log(this.api); //it's work.
+    this.api.executeCommand('displayName', this.nickName);
+    // this.options = {
+    //   roomName: "JordansMeeting1",
+    //   width: 700,
+    //   height: 700,
+    //   parentNode: document.querySelector('#meet')
+    // }
+
+    // this.api = new JitsiMeetExternalAPI(this.domain, this.options);
   }
 
 }
